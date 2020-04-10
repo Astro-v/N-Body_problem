81c0a34113739e5ec1583403268f428949f74c2c
JebrilYousfi <62743508+JebrilYousfi@users.noreply.github.com>
2020-04-10 16:01:31 +0200

    Creation of the Body class
    We need to see if import Vector is the right syntax, if print on a Vector works in a function that isn't in the Vector class, and if the \n works to have a clear display of the caracteristics of the body.

--- Body.py
+++ Body.py
@@ -1,1 +1,11 @@
+import Vector
+
 class Body:
+    def __init__(self,p0,v0,m,radius):
+        self.p = [p0]
+        self.v = v0
+        self.m = m
+        self.radius = radius
+        
+    def __str__(self):
+        return "Its current position is "+print(self.p[-1])+" (en pc).\n""Its current speed is "+print(self.v)+" (en km.s-1).\n""Its mass is "+self.m+" kg.\n""Its radius is "+self.radius+" km."
