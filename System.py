706251f90ffcbb6b71f7449e7c072634e8a6fbdb
Astro-v <58807043+Astro-v@users.noreply.github.com>
2020-04-10 15:46:44 +0200

    Update System.py

--- System.py
+++ System.py
@@ -5,7 +5,7 @@ import Body
 the system class manages all the N-body system and manages the simulation
 '''
 class System:
-    def __init__(self,p0,v0,m,radius):
+    def __init__(self,p0,v0,m,radius,dt):
         ''' 
         Initialize the N-Body system
         p0,v0,m,radius are array that contain:
@@ -13,8 +13,11 @@ class System:
         v0 : initial velocity
         m : mass
         radius : radius of the several body
+        dt : infinitesimal time
         '''
         
+        self.t = 0
+        self.dt = dt
         self.N = len(p0)
         self.body = []
         for i in range(0,self.N,1):
@@ -34,6 +37,5 @@ class System:
         self.N = self.N +1
         self.body.append(Body(p0,v0,m,radius))
        
-        
-        
-        
+     
+     
