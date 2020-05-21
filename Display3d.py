295ce4a4aae2264151985bb76951bee840050a70
Astro-v <58807043+Astro-v@users.noreply.github.com>
2020-05-21 19:10:04 +0200

    Update Display3D

--- Display3d.py
+++ Display3d.py
@@ -1,3 +1,6 @@
+#### JEBRIL & VALENTIN ####
+#### -- DISPLAY3D.PY --####
+
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
@@ -5,21 +8,21 @@ from OpenGL.GLU import *
 import math
 import numpy as np
 from typing import List
-
 from System import *
 from Body import *
 from Vector import *
 
 
-
+#-------- CONSTANT --------#
 BLACK = [0, 0, 10]
 WHITE = [255, 255, 255]
 RED = [255, 0, 0]
 YELLOW = [150,150,0]
 GREEN = [0, 150, 0]
 BLUE = [0, 0, 255]
+ORANGE = [255, 150, 0]
 
-def sphere(center: List[int],color: List[int],radius: float = 1):
+def sphere(center: List[int],color: List[int],radius: float = 1): # Draw 3D sphere shape
 	glBegin(GL_POLYGON)
 	theta = -math.pi
 	thetaList = np.linspace(-math.pi,+math.pi,20)
@@ -29,14 +32,8 @@ def sphere(center: List[int],color: List[int],radius: float = 1):
 			glColor3f(color[0],color[1],color[2])
 			glVertex3f(center[0]+radius*math.sin(theta)*math.cos(phi),center[1]+radius*math.sin(theta)*math.sin(phi),center[2]+radius*math.cos(theta))
 	glEnd()
-	
-def point(pos,color):
-	glBegin(GL_POINTS)
-	glColor3f(color[0],color[1],color[2])
-	glVertex3f(pos[0],pos[1],pos[2])
-	glEnd()
 
-def grid(X):
+def grid(X): # Draw the several axes of the plan
 	glBegin(GL_POINTS)
 	x = np.linspace(-2*X,2*X,200)
 	for i in range(len(x)):
@@ -46,18 +43,34 @@ def grid(X):
 		glVertex3f(0,0,x[i])
 	glEnd()
 
-def drawText(position, textString):     
+def point(pos: list,color: list): # Draw a point in the plan
+	glBegin(GL_POINTS)
+	glColor3f(color[0],color[1],color[2])
+	glVertex3f(pos[0],pos[1],pos[2])
+	glEnd()
+
+def drawText(position: list, textString: str):     
     font = pygame.font.Font (None, 32)
     textSurface = font.render(textString, True, (255,255,255,255), (0,0,0,255))     
     textData = pygame.image.tostring(textSurface, "RGBA", True)     
     glRasterPos3d(*position)     
     glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
 
+def axes(theta: float,phi: float):
+    eR = [-math.cos(phi)*math.sin(theta),+math.sin(phi),-math.cos(phi)*math.cos(theta)]
+    eTheta = [-math.cos(theta),0,+math.sin(theta)]
+    ePhi = [-math.sin(phi)*math.sin(theta),-math.cos(phi),-math.sin(phi)*math.cos(theta)]
+    eZ = [math.cos(phi)*ePhi[0]+math.sin(phi)*eR[0],math.cos(phi)*ePhi[1]+math.sin(phi)*eR[1],math.cos(phi)*ePhi[2]+math.sin(phi)*eR[2]]
+    return eR,eTheta,ePhi,eZ
+				
+
 def display(sys: System,method: str,xyzmax: float = 260000000.0,step: int = 200,ref: int = 0):
 	"""
 	sys: The system to display
 	method: chose between two method "Euler" (default) or "RK4"
 	"""
+
+	#-------- INITIALIZATION --------#
 	X = 800
 	Y = 800
 	Z = 800
@@ -82,41 +95,41 @@ def display(sys: System,method: str,xyzmax: float = 260000000.0,step: int = 200,
 	ePhi = [0,1,0]
 	phi = 0
 	theta = 0
-	eR = [-math.cos(phi)*math.sin(theta),+math.sin(phi),-math.cos(phi)*math.cos(theta)]
-	eTheta = [-math.cos(theta),0,+math.sin(theta)]
-	ePhi = [-math.sin(phi)*math.sin(theta),-math.cos(phi),-math.sin(phi)*math.cos(theta)]
-	eZ = [math.cos(phi)*ePhi[0]+math.sin(phi)*eR[0],math.cos(phi)*ePhi[1]+math.sin(phi)*eR[1],math.cos(phi)*ePhi[2]+math.sin(phi)*eR[2]]
+	eR,eTheta,ePhi,eZ = axes(theta,phi)
 	posText = [eTheta[0]*(X-10)+ePhi[0]*(-Y+50),eTheta[1]*(X-10)+ePhi[1]*(-Y+50),eTheta[2]*(X-10)+ePhi[2]*(-Y+50)]
-	angle = math.pi/500
+	
+	angle = math.pi/500 # angle step for rotation
 	glTranslatef(0.0,0.0,-Z) # initial position
 	pygame.key.set_repeat(5, 5) # key repeat
 
-
+	#-------- MAIN LOOP --------#
 	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+	    #-------- EVENT --------#
+		for event in pygame.event.get(): # There is some event 
+			if event.type == pygame.QUIT: # Here the user clic on the quit button
 				pygame.quit()
 				quit()
-			elif event.type == KEYDOWN:
-				eR = [-math.cos(phi)*math.sin(theta),+math.sin(phi),-math.cos(phi)*math.cos(theta)]
-				eTheta = [-math.cos(theta),0,+math.sin(theta)]
-				ePhi = [-math.sin(phi)*math.sin(theta),-math.cos(phi),-math.sin(phi)*math.cos(theta)]
-				eZ = [math.cos(phi)*ePhi[0]+math.sin(phi)*eR[0],math.cos(phi)*ePhi[1]+math.sin(phi)*eR[1],math.cos(phi)*ePhi[2]+math.sin(phi)*eR[2]]
-				if  event.key == K_LEFT:
+			elif event.type == KEYDOWN: # Here the user push a key on the keyboard
+				eR,eTheta,ePhi,eZ = axes(theta,phi)
+
+				if  event.key == K_LEFT: # Here the user clic on the key left
 					glRotatef(-angle*180/math.pi, eZ[0],eZ[1],eZ[2])
 					glRotatef(angle*180/math.pi*2*math.sin(phi),eR[0],eR[1],eR[2])
 					theta -= angle
-				elif  event.key == K_RIGHT:
+
+				elif  event.key == K_RIGHT: # Here the user clic on the key right
 					glRotatef(angle*180/math.pi, eZ[0],eZ[1],eZ[2])
 					glRotatef(-angle*180/math.pi*2*math.sin(phi),eR[0],eR[1],eR[2])
 					theta += angle
-				elif  event.key == K_UP:
+
+				elif  event.key == K_UP: # Here the user clic on the key up
 					glRotatef(-angle*180/math.pi, eTheta[0],eTheta[1],eTheta[2])
 					phi -= angle
-				elif  event.key == K_DOWN:
+
+				elif  event.key == K_DOWN: # Here the user clic on the key down
 					glRotatef(angle*180/math.pi, eTheta[0],eTheta[1],eTheta[2])
 					phi += angle
-				elif event.key == K_0:
+				elif event.key == K_0: # Here the user clic on the key 0
 					ref = 0
 				elif event.key == K_1:
 					if sys.N > 1:
@@ -150,13 +163,13 @@ def display(sys: System,method: str,xyzmax: float = 260000000.0,step: int = 200,
 						xyzmax -=  1000000
 				elif event.key == K_s:
 					xyzmax += 1000000
-			elif event.type == MOUSEBUTTONUP and event.button == 4:
+			elif event.type == MOUSEBUTTONUP and event.button == 4: # Here the user roll the mouse wheel
 				if xyzmax >= 20000000:
 					xyzmax -=  3000000
 			elif event.type == MOUSEBUTTONUP and event.button == 5:
 				xyzmax += 3000000
 
-
+		#-------- DISPLAY --------#
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 		grid(X)
 		for j in range(sys.N):
@@ -178,8 +191,8 @@ def display(sys: System,method: str,xyzmax: float = 260000000.0,step: int = 200,
 				Xt[j]=X*sys.body[j].p.x/xyzmax
 				Yt[j]=Y*sys.body[j].p.y/xyzmax
 				Zt[j]=Z*sys.body[j].p.z/xyzmax
-		eTheta = [-math.cos(theta),0,+math.sin(theta)]
-		ePhi = [-math.sin(phi)*math.sin(theta),-math.cos(phi),-math.sin(phi)*math.cos(theta)]
+
+		eR,eTheta,ePhi,eZ = axes(theta,phi)
 		posText = [eTheta[0]*(X-10)+ePhi[0]*(-Y+50),eTheta[1]*(X-10)+ePhi[1]*(-Y+50),eTheta[2]*(X-10)+ePhi[2]*(-Y+50)]
 		drawText(posText,str(y(sys.t))+"y "+str(d(sys.t))+"d "+str(h(sys.t))+"h")
 		pygame.display.flip()
@@ -193,3 +206,4 @@ def d(sec):
 
 def h(sec):
 	return (sec//(3600))%24
+
